6e3a17101a9be963fc2da08965b63027d63f9b63
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2023-03-29 15:58:16 -0300

    Forms with a Insert function

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+const bodyParser = require("body-parser")
 const express = require("express")
 const app = express()
+const post = require("./models/post")
 
 const handlebars = require("express-handlebars").engine
 
 app.engine("handlebars", handlebars({defaultLayout:"main"}))
 app.set("view engine", "handlebars")
+
+app.use(bodyParser.urlencoded({extend: false}))
+app.use(bodyParser.json())
+
 
 //---------------------------------------------------------------/
 //criando uma rota para o diretório principal do domínio
@@ -16,6 +22,21 @@ app.get("/", function(req, res) {
 //criando uma rota para a segunda página
 app.get("/cadastrar", function(req, res) {
     res.send("Formulário recebido!!")
+})
+
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data: req.body.data,
+        observacao: req.body.observacao
+        
+    }).then(function(){
+        res.send("Dados enviados com sucesso!")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: "+erro)
+    });
 })
 
 //---------------------------------------------------------------/
